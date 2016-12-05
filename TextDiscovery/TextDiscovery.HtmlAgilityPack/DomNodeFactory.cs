da3065ac323a39a1e40a940e76f67a603e589005
David West <dwest21@lsu.edu>
2016-12-05 08:56:51 -0600

    completed HtmlAgilityPack adapters; all versions -> 1.0.3

---
+++ TextDiscovery/TextDiscovery.HtmlAgilityPack/DomNodeFactory.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections.Generic;
+using HtmlAgilityPack;
+
+namespace TextDiscovery.Dom.HtmlAgilityPack
+{
+	public class DomNodeFactory : IDomNodeFactory<HtmlNode>
+	{
+		private static readonly Dictionary<string, string> EmptyAttributes = new Dictionary<string, string>();
+
+		public HtmlNode CreateTextNode(HtmlNode root, string text)
+		{
+			return HtmlNode.CreateNode("text");
+		}
+
+		public HtmlNode CreateElementNode(HtmlNode root, string elementName, Dictionary<string, string> attributes, string innerHtml = null)
+		{
+			var node =  HtmlNode.CreateNode($"<{elementName}>{innerHtml}</{elementName}>");
+
+			foreach (var attr in attributes)
+			{
+				node.SetAttributeValue(attr.Key, attr.Value);
+			}
+
+			return node;
+		}
+
+		public HtmlNode CreateElementNode(HtmlNode root, string elementName, string innerHtml = null)
+		{
+			return CreateElementNode(root, elementName, EmptyAttributes, innerHtml);
+		}
+	}
+}
---
+++ TextDiscovery/TextDiscovery.HtmlAgilityPack/HtmlConverter.cs
@@ -0,0 +1,13 @@
+﻿
+using HtmlAgilityPack;
+using TextDiscovery.Html;
+
+namespace TextDiscovery.HtmlAgilityPack
+{
+	public class HtmlConverter : IHtmlConverter<HtmlNode>
+	{
+		public HtmlNode ConvertToNode(string html) => HtmlNode.CreateNode(html);
+
+		public string Render(HtmlNode node) => node.OuterHtml;
+	}
+}
